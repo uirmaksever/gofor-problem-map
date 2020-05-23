dbd1738d581fea2aa0c3f939c920b4e5659168fa
Umut Irmaksever <umutirmaksever@gmail.com>
2020-05-24 00:08:35 +0300

    textarea autosize added

--- gofor_problem_map/static/js/project.js
+++ gofor_problem_map/static/js/project.js
@@ -1,1 +1,10 @@
 /* Project specific Javascript goes here. */
+
+// Makes all textareas responsive to input
+$('textarea').each(function () {
+  this.setAttribute('rows', 1);
+  this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+}).on('input', function () {
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + 'px';
+});
