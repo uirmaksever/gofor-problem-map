c1fab281e4462fe895ac5a719a5fca9643a7d7ad
Umut Irmaksever <umutirmaksever@gmail.com>
2020-05-28 22:30:48 +0300

    verbose names

--- gofor_problem_map/problem_map/models.py
+++ gofor_problem_map/problem_map/models.py
@@ -19,6 +19,10 @@ class Country(gis_models.Model):
     def __str__(self):
         return self.name_0
 
+    class Meta:
+        verbose_name = "Ülke"
+        verbose_name_plural = "Ülkeler"
+
 
 class Province(gis_models.Model):
     id = gis_models.IntegerField(primary_key=True)
@@ -37,6 +41,10 @@ class Province(gis_models.Model):
 
     def __str__(self):
         return self.name_1
+
+    class Meta:
+        verbose_name = "İl"
+        verbose_name_plural = "İller"
 
 
 class District(gis_models.Model):
@@ -60,36 +68,45 @@ class District(gis_models.Model):
     def __str__(self):
         return "{} - {}".format(self.name_1, self.name_2)
 
-
-
-
+    class Meta:
+        verbose_name = "İlçe"
+        verbose_name_plural = "İlçeler"
 
 
 class ThematicField(models.Model):
-    name = models.CharField(max_length=DEFAULT_CHAR_LENGTH)
-    description = RichTextField(null=True, blank=True)
+    name = models.CharField(max_length=DEFAULT_CHAR_LENGTH, verbose_name="Tematik Alan İsmi")
+    description = RichTextField(null=True, blank=True, verbose_name="Açıklama")
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Tematik Alan"
+        verbose_name_plural = "Tematik Alanlar"
+
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=DEFAULT_CHAR_LENGTH)
-    last_name = models.CharField(max_length=DEFAULT_CHAR_LENGTH)
-    ip = models.GenericIPAddressField(null=True, blank=True)
-    could_contact = models.BooleanField()
-    email = models.EmailField(null=True, blank=True)
-    phone_number = PhoneNumberField(null=True, blank=True)
+    first_name = models.CharField(max_length=DEFAULT_CHAR_LENGTH, verbose_name="İsim")
+    last_name = models.CharField(max_length=DEFAULT_CHAR_LENGTH, verbose_name="Soyisim")
+    ip = models.GenericIPAddressField(null=True, blank=True, verbose_name="IP Adresi")
+    could_contact = models.BooleanField(verbose_name="İletişim İzni")
+    email = models.EmailField(null=True, blank=True, verbose_name="E-Posta Adresi")
+    phone_number = PhoneNumberField(null=True, blank=True, verbose_name="Telefon No")
 
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)
 
+    class Meta:
+        verbose_name = "Kişi"
+        verbose_name_plural = "Kişiler"
+
+
 class ProblemType(models.Model):
-    name = models.TextField()
-    thematic_field = models.ManyToManyField(ThematicField)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_approved = models.BooleanField(default=False)
+    name = models.TextField(verbose_name="Problem Çeşidi Tanımı")
+    thematic_field = models.ManyToManyField(ThematicField, verbose_name="Tematik Alan")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Eklenme Tarihi")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Güncellenme Tarihi")
+    is_approved = models.BooleanField(default=False, verbose_name="Onay")
 
     def __str__(self):
         return self.name
@@ -97,18 +114,23 @@ class ProblemType(models.Model):
     def get_absolute_url(self):
         return reverse("problem_map:problemtype-detail", args=[str(self.pk)])
 
+    class Meta:
+        verbose_name = "Problem Çeşidi"
+        verbose_name_plural = "Problem Çeşitleri"
+
+
 class Problem(models.Model):
-    name = models.CharField(max_length=1000)
-    description = models.TextField()
-    related_problem_type = models.ForeignKey(ProblemType, on_delete=models.CASCADE)
-    location = gis_models.PointField()
-    occurrence_date = models.DateField()
+    name = models.CharField(max_length=1000, verbose_name="Problem Tanımı")
+    description = models.TextField(verbose_name="Açıklama")
+    related_problem_type = models.ForeignKey(ProblemType, on_delete=models.CASCADE, verbose_name="İlgili Problem Çeşidi")
+    location = gis_models.PointField(verbose_name="Konum")
+    occurrence_date = models.DateField(verbose_name="Gerçekleşme Tarihi")
     # thematic_field = models.ManyToManyField(ThematicField)
-    related_person = models.OneToOneField(Person, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_approved = models.BooleanField(default=False)
-    related_district = models.ForeignKey(District, on_delete=models.CASCADE)
+    related_person = models.OneToOneField(Person, on_delete=models.CASCADE, verbose_name="İlgili Kişi")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Eklenme Tarihi")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Güncellenme Tarihi")
+    is_approved = models.BooleanField(default=False, verbose_name="Onay")
+    related_district = models.ForeignKey(District, on_delete=models.CASCADE, verbose_name="İlçe")
 
     def __str__(self):
         return "[{}] {} - {}".format(
@@ -120,3 +142,6 @@ class Problem(models.Model):
     def get_absolute_url(self):
         return reverse("problem_map:problem-detail", args=[str(self.pk)])
 
+    class Meta:
+        verbose_name = "Problem"
+        verbose_name_plural = "Problemler"
