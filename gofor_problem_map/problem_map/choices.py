677f1c0a0d2f905fb1ed27b192c0e057908e7c09
Umut Irmaksever <umutirmaksever@gmail.com>
2020-09-30 20:12:55 +0300

    age limit abolished, additional sex options added

--- gofor_problem_map/problem_map/choices.py
+++ gofor_problem_map/problem_map/choices.py
@@ -1,5 +1,7 @@
 RELATED_PERSON_SEX_CHOICES = (
     ("M", "Erkek"),
     ("F", "Kadın"),
+    ("NB", "Non-binary (ikili olmayan cinsiyet)"),
+    ("Other", "Diğer"),
     ("NA", "Belirtmek istemiyorum")
 )
